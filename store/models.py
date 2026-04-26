d2bed0bbf57df4509fce3c9985d3179ab491e715
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-04-27 00:53:58 +0100

    apps Setting

--- store/models.py
+++ store/models.py
@@ -3,16 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    titile = models.CharField(max_length=255)
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     def __str__(self):
         return self.title
 
-class Costomer(models.Model):
+class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
