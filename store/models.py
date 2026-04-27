c912b835288c6afb104de619b9fd36056f956399
Alkama Umar Liman <alkalineumarliman@gmail.com>
2026-04-27 13:33:24 +0100

    onelin

--- store/models.py
+++ store/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -27,18 +28,29 @@ class Product(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotion = models.ManyToManyField(Promotion, blank=True)
+    
+    class Meta:
+        ordering = ['title']
+        indexes = [
+            models.Index(fields=['title']),
+        ]
 
-    def  __str__(self):
+    def __str__(self):
         return self.title
-
-
+    
+    
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
-
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
+        indexes = [
+            models.Index(fields=['first_name', 'last_name']),
+        ]
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
